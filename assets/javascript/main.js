1979830596fdde277366b0be5344d601aa1a8681
ManuelTrappolini <manuel.trappolini@gmail.com>
2024-09-30 15:42:07 +0200

    add third fase qith condition

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -10,9 +10,30 @@ console.log('ciao');
 //Creo veriabili per i chilometri e per eta' passeggero
 let kilometers
 let age 
+let price
 
 //2 Fase di raccoglimento dati
 // creo prompt per richiedere all'utente i chilometri e eta' del passeggero
 
 kilometers = prompt('Inserisci il numero di chilometri che vuoi percorrere')
 age = prompt('inserisci qui la tua etá')
+price = 0.21  * kilometers
+// console.log(price)
+
+//3 fase di elaborazione dati
+// creo condizioni in base agli sconti applicabili in base all'eta' del passeggero
+
+if (age < 18){
+    discountUnder = price - ((price * 20) / 100) 
+    console.log(discountUnder)
+    alert (`il tuo prezzo é di € ${discountUnder}`)
+}
+else if (age > 65){
+    discountOver = price - ((price * 40) / 100)
+    console.log(discountOver);
+    alert (`il tuo prezzo é di € ${discountOver}`)
+}
+
+else{
+    console.log(price)
+}
